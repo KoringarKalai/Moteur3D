39a39be27357627a8157ce9d596124b08a21f6a1
Koringar <alexisbarthelemy54@yahoo.fr>
2019-02-06 17:46:31 +0100

    Rendu anaglyphe

--- Moteur3D.cpp
+++ Moteur3D.cpp
@@ -16,6 +16,7 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
+int anaglyph = 1;
 
 struct VectF {
 	float x, y, z;
@@ -52,9 +53,93 @@ VectF crossProduct(VectF v1, VectF v2) {
 VectF barycentric(VectF p0, VectF p1, VectF p2, VectF p) {
 	VectF v1 = VectF(p2.x - p0.x, p1.x - p0.x, p0.x - p.x);
 	VectF v2 = VectF(p2.y - p0.y, p1.y - p0.y, p0.y - p.y);
-	VectF cp = crossProduct(v1,v2);
+	VectF cp = crossProduct(v1, v2);
 	if (std::abs(cp.z) > 1e-2) return VectF(1.f - (cp.x + cp.y) / cp.z, cp.y / cp.z, cp.x / cp.z);
 	return VectF(-1, 1, 1);
+}
+
+struct Matrix {
+	std::vector<std::vector<float>> m;
+	int row, col;
+
+	Matrix(int r, int c) : m(std::vector<std::vector<float> >(r, std::vector<float>(c, 0.f))), row(r), col(c) { }
+
+	void identity() {
+		for (int i = 0; i < row; ++i) {
+			for (int j = 0; j < col; j++) {
+				if (i == j)
+					m[i][j] = 1;
+				else 
+					m[i][j] = 0;
+			}
+		}
+	}
+};
+
+Matrix mult(Matrix m1, Matrix m2) {
+	Matrix res = Matrix(m1.row, m2.col);
+	for (int i = 0; i < m1.row; i++) {
+		for (int j = 0; j < m2.col; j++) {
+			res.m[i][j] = 0.f;
+			for (int k = 0; k < m1.row; k++) {
+				res.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
+	return res;
+}
+
+Matrix lookAt(VectF eye, VectF center, VectF v_up) {
+	VectF z = VectF(eye.x - center.x, eye.y - center.y, eye.z - center.z);
+	z.normalize();
+	VectF x = crossProduct(v_up, z);
+	x.normalize();
+	VectF y = crossProduct(z, x);
+	y.normalize();
+
+	Matrix res = Matrix(4, 4);
+	res.identity();
+	
+	res.m[0][0] = x.x;
+	res.m[0][1] = x.y;
+	res.m[0][2] = x.z;
+	res.m[1][0] = y.x;
+	res.m[1][1] = y.y;
+	res.m[1][2] = y.z;
+	res.m[2][0] = z.x;
+	res.m[2][1] = z.y;
+	res.m[2][2] = z.z;
+	res.m[0][3] = -center.x;
+	res.m[1][3] = -center.y;
+	res.m[2][3] = -center.z;
+
+	return res;
+}
+
+Matrix viewPort(int x, int y, int w, int h, int d) {
+	Matrix m = Matrix(4,4);
+	m.identity();
+	m.m[0][3] = x + w / 2.f;
+	m.m[1][3] = y + h / 2.f;
+	m.m[2][3] = d / 2.f;
+
+	m.m[0][0] = w / 2.f;
+	m.m[1][1] = h / 2.f;
+	m.m[2][2] = d / 2.f;
+	return m;
+}
+
+Matrix v2m(VectF v) {
+	Matrix m = Matrix(4, 1);
+	m.m[0][0] = v.x;
+	m.m[1][0] = v.y;
+	m.m[2][0] = v.z;
+	m.m[3][0] = 1;
+	return m;
+}
+
+VectF m2v(Matrix m) {
+	return VectF(m.m[0][0] / m.m[3][0], m.m[1][0] / m.m[3][0], m.m[2][0] / m.m[3][0]);
 }
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
@@ -88,7 +173,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	}
 }
 
-void fillTriangle(VectF p0, VectF p1, VectF p2, TGAImage &image, TGAImage &imageTexture, VectF c0, VectF c1, VectF c2, float teinte, float **zbuffer) {
+void fillTriangle(VectF p0, VectF p1, VectF p2, TGAImage &image, TGAImage &imageTexture, TGAImage &imageNm, VectF c0, VectF c1, VectF c2, float **zbuffer) {
 	int xmin = std::min(std::min(p0.x,p1.x),p2.x);
 	int xmax = std::max(std::max(p0.x, p1.x), p2.x);
 	int ymin = std::min(std::min(p0.y, p1.y), p2.y);
@@ -102,11 +187,37 @@ void fillTriangle(VectF p0, VectF p1, VectF p2, TGAImage &image, TGAImage &image
 			P.z += p0.z * bary.x + p1.z * bary.y + p2.z * bary.z;
 			if (zbuffer[(int)P.x][(int)P.y] < P.z) {
 				zbuffer[(int)P.x][(int)P.y] = P.z;
+
 				float xc = c0.x * bary.x + c1.x * bary.y + c2.x * bary.z;
 				float yc = c0.y * bary.x + c1.y * bary.y + c2.y * bary.z;
+
 				TGAColor couleur = imageTexture.get(xc * 1024 , yc * 1024);
-				for (int i = 0; i < 3; i++) couleur.raw[i] *= teinte;
-				image.set((int)P.x, (int)P.y, couleur);
+				TGAColor colorNm = imageNm.get(xc * 1024, yc * 1024);
+
+				VectF color = VectF((float)colorNm.r /128 - 1, (float)colorNm.g / 128 - 1, (float)colorNm.b / 128 - 1);
+				VectF eye = VectF(0,0,1);
+				eye.normalize();
+
+				float intensity = (eye.x * color.x + eye.y * color.y + eye.z * color.z);
+
+				intensity = std::max(std::min(intensity, 1.f), 0.f);
+				for (int i = 0; i < 3; i++) couleur.raw[i] *= intensity;
+				
+				// Anaglyph
+				if (anaglyph == 1) {
+					intensity = 0.2126 * couleur.raw[0] + 0.7152 * couleur.raw[1] + 0.0722 * couleur.raw[2];
+					TGAColor oldColor = image.get((int)P.x, (int)P.y);
+					oldColor.raw[0] = intensity;
+					image.set((int)P.x, (int)P.y, oldColor);
+				} else {
+					intensity = 0.2126 * couleur.raw[0] + 0.7152 * couleur.raw[1] + 0.0722 * couleur.raw[2];
+					TGAColor oldColor = image.get((int)P.x + 60, (int)P.y);
+					oldColor.raw[2] = intensity;
+					image.set((int)P.x + 60, (int)P.y, oldColor);
+
+				}
+				// Normal 
+				//image.set((int)P.x, (int)P.y, couleur);
 			}
 		}
 	}
@@ -118,11 +229,21 @@ void drawFile(int width, int height, int profondeur, TGAImage &image, TGAColor c
 	std::vector<std::vector<float>> triangle;
 	std::vector<std::vector<float>> texture;
 	TGAImage imageTexture = TGAImage();
+	TGAImage imageNm = TGAImage();
+	TGAImage imageSpec = TGAImage();
+
 	//std::ifstream infile("obj/african_head/african_head.obj");
 	//imageTexture.read_tga_file("obj/african_head/african_head_diffuse.tga");
+	//imageNm.read_tga_file("obj/african_head/african_head_nm.tga");
+
 	std::ifstream infile("obj/diablo3_pose/diablo3_pose.obj");
 	imageTexture.read_tga_file("obj/diablo3_pose/diablo3_pose_diffuse.tga");
+	imageNm.read_tga_file("obj/diablo3_pose/diablo3_pose_nm.tga");
+	//imageSpec.read_tga_file("obj/diablo3_pose/diablo3_pose_spec.tga");
+	
 	imageTexture.flip_vertically();
+	imageNm.flip_vertically();
+	imageSpec.flip_vertically();
 	std::string line;
 	int i = 0;
 	int j = 0;
@@ -168,57 +289,56 @@ void drawFile(int width, int height, int profondeur, TGAImage &image, TGAColor c
 		int ix0 = triangle[i][0];
 		int ix1 = triangle[i][1];
 		int ix2 = triangle[i][2];
+		VectF p0 = VectF(
+			(points[ix0].x), 
+			(points[ix0].y),
+			(points[ix0].z));
+		VectF p1 = VectF(
+			(points[ix1].x),
+			(points[ix1].y),
+			(points[ix1].z));
+		VectF p2 = VectF(
+			(points[ix2].x),
+			(points[ix2].y),
+			(points[ix2].z));
+
+		int ix0T = texture[i][0];
+		int ix1T = texture[i][1];
+		int ix2T = texture[i][2];
+		VectF c0 = VectF(colors[ix0T].x, colors[ix0T].y, colors[ix0T].z);
+		VectF c1 = VectF(colors[ix1T].x, colors[ix1T].y, colors[ix1T].z);
+		VectF c2 = VectF(colors[ix2T].x, colors[ix2T].y, colors[ix2T].z);
+
+		float eyesep = 0.1;
+		VectF eye = VectF(1 - eyesep, 0, 1);
+		if (anaglyph == 1)
+			eye = VectF(eye.x + eyesep, eye.y, eye.z);
+		eye.normalize();
+		VectF center = VectF(0, 0, 0);
+		Matrix ModelView = lookAt(eye,center, VectF(0, 1, 0));
+		VectF n = VectF(eye.x - center.x, eye.y - center.y, eye.z - center.z);
+		Matrix Projection = Matrix(4, 4);
+		Projection.identity();
+		Projection.m[3][2] = -1.f / n.norme();
+		Matrix ViewPort = viewPort(width / 8, height / 8, width * 3 / 4, height * 3 / 4, profondeur * 3 / 4);
 		
-		// VectFeur p0p1
-		VectF v1 = VectF(
-			points[ix1].x - points[ix0].x,
-			points[ix1].y - points[ix0].y,
-			points[ix1].z - points[ix0].z
-		);
-		// VectFeur p0p2
-		VectF v2 = VectF(
-			points[ix2].x - points[ix0].x,
-			points[ix2].y - points[ix0].y,
-			points[ix2].z - points[ix0].z
-		);
-		// Produit VectForiel v1 x v2
-		VectF pv = crossProduct(v1,v2);
-
-		// Produit scalaire    VectFCamera . (v1 x v2)
-		VectF VectCamera = VectF(0, 0, 1);
-		pv.normalize();
-		float produit = (VectCamera.x * pv.x + VectCamera.y * pv.y + VectCamera.z * pv.z);
-		// Si le produit est positif on dessine sinon non 
-		if (produit > 0) {
-			float teinte = produit;
-			VectF p0 = VectF(
-				int(points[ix0].x * width / 2 + width / 2), 
-				int(points[ix0].y * height / 2 + height / 2),
-				int(points[ix0].z * profondeur / 2 + profondeur / 2));
-			VectF p1 = VectF(
-				int(points[ix1].x * width / 2 + width / 2),
-				int(points[ix1].y * height / 2 + height / 2),
-				int(points[ix1].z * profondeur / 2 + profondeur / 2));
-			VectF p2 = VectF(
-				int(points[ix2].x * width / 2 + width / 2),
-				int(points[ix2].y * height / 2 + height / 2),
-				int(points[ix2].z * profondeur / 2 + profondeur / 2));
-
-			int ix0T = texture[i][0];
-			int ix1T = texture[i][1];
-			int ix2T = texture[i][2];
-			VectF c0 = VectF(colors[ix0T].x, colors[ix0T].y, colors[ix0T].z);
-			VectF c1 = VectF(colors[ix1T].x, colors[ix1T].y, colors[ix1T].z);
-			VectF c2 = VectF(colors[ix2T].x, colors[ix2T].y, colors[ix2T].z);
-			fillTriangle(p0, p1, p2, image, imageTexture, c0, c1, c2, teinte, zbuffer);
-		}
+		Matrix vpp = mult(ViewPort, Projection);
+		Matrix vppvm = mult(vpp, ModelView);
+		p0 = m2v(mult(vppvm, v2m(p0)));
+		p1 = m2v(mult(vppvm, v2m(p1)));
+		p2 = m2v(mult(vppvm, v2m(p2)));
+		
+		fillTriangle(p0, p1, p2, image, imageTexture, imageNm, c0, c1, c2, zbuffer);
 	}
 }
 
 int main(int argc, char** argv) {
-	int h = 2000, w = 2000, p = 2000;
-	TGAImage image(h, w, TGAImage::RGB);
+	int h = 2000, w = 2000, p = 255;
+	TGAImage image(h + 60, w, TGAImage::RGB);
 	drawFile(h, w, p, image, white);
+	anaglyph = 0;
+	drawFile(h, w, p, image, white);
+
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
